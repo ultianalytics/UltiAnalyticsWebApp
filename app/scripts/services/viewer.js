ae9a05fac059e454184211c8b47a2c79e54c64fd
Kyle Geppert <Kyle.Geppert@docusign.com>
2014-04-29 22:42:53 -0700

    Attempt to fix the tablet problems.

--- app/scripts/services/viewer.js
+++ app/scripts/services/viewer.js
@@ -4,7 +4,7 @@ angular.module('newBetaApp')
   .factory('viewer', function () {
     return {
       isMobile: function () {
-        return /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        return $(window).outerWidth() <= 768;
       }
     };
   });
