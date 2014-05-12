b6fc6e72b9a2adf96b6bebd14e5d67bf1ca77493
Kyle Geppert <Kyle.Geppert@docusign.com>
2014-05-11 22:43:45 -0700

    Added page views to google analytics.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,29 +13,25 @@ angular.module('newBetaApp', [
         next.set($location.path());
         return Authorization.ping($location.url().match(/^\/\d+/)[0].slice(1));
       }
-      // function checkPlayerExistence ($q, $location, api) {
-      //   api.retrieveTeam($location.url().match(/^\/\d+/)[0].slice(1), true, function(response){
-      //     if (_(response.players).pluck('name').contains(decodeURI($location.url().match(/\/[^\/]+$/)[0].slice(1)))){
-      //       deferred.resolve();
-      //     } else {
-      //       $location.url('/404');
-      //       deferred.resolve(new Error('bad player name'));
-      //     }
-      //   });
-      //   var deferred = $q.defer();
-      //   return deferred.promise;
-      // }
-
+      function logRoute($location){
+        if (_.contains($location.host().toLowerCase(), 'ultimate-numbers') || _.contains($location.host().toLowerCase(), 'ultianalytics')){
+          var pageRoute = $location.url().replace(/\/\d+\//, '').match(/\w+/)[0]
+          ga('send', 'pageview', {
+            'page': pageRoute,
+            'title': pageRoute.slice(0,1).toUpperCase() + pageRoute.slice(1)
+          });
+        }
+      }
 
       $routeProvider
-        .when('/', {templateUrl: 'views/splash.html'})
-        .when('/:teamId/login', {templateUrl: 'views/login.html', controller: 'LoginCtrl'})
-        .when('/:teamId/players', {templateUrl: 'views/players.html', controller: 'PlayersCtrl', resolve: {authorized: checkAuth}})
-        .when('/:teamId/team', {templateUrl: 'views/team.html', controller: 'TeamCtrl', resolve: {authorized: checkAuth}})
-        .when('/:teamId/download', {templateUrl: 'views/download.html', controller: 'DownloadCtrl', resolve: {authorized: checkAuth}})
-        .when('/:teamId/line', {templateUrl: 'views/line.html', controller: 'LineCtrl', resolve: {authorized: checkAuth}})
-        .when('/:teamId/games', {templateUrl: 'views/games.html', controller: 'GamesCtrl', resolve: {authorized: checkAuth}})
-        .when('/:teamId/player/:playerNameUri', {templateUrl: 'views/player.html', controller: 'PlayerCtrl', resolve: { authorized: checkAuth}})
+        .when('/', {templateUrl: 'views/splash.html', resolve: {ga: logRoute}})
+        .when('/:teamId/login', {templateUrl: 'views/login.html', controller: 'LoginCtrl', resolve: {ga: logRoute}})
+        .when('/:teamId/players', {templateUrl: 'views/players.html', controller: 'PlayersCtrl', resolve: {authorized: checkAuth, ga: logRoute}})
+        .when('/:teamId/team', {templateUrl: 'views/team.html', controller: 'TeamCtrl', resolve: {authorized: checkAuth, ga: logRoute}})
+        .when('/:teamId/download', {templateUrl: 'views/download.html', controller: 'DownloadCtrl', resolve: {authorized: checkAuth, ga: logRoute}})
+        .when('/:teamId/line', {templateUrl: 'views/line.html', controller: 'LineCtrl', resolve: {authorized: checkAuth, ga: logRoute}})
+        .when('/:teamId/games', {templateUrl: 'views/games.html', controller: 'GamesCtrl', resolve: {authorized: checkAuth, ga: logRoute}})
+        .when('/:teamId/player/:playerNameUri', {templateUrl: 'views/player.html', controller: 'PlayerCtrl', resolve: { authorized: checkAuth, ga: logRoute}})
         .when('/404', {templateUrl: 'views/404.html', controller: '404Ctrl'})
         .otherwise({redirectTo: '/404'});
     }
