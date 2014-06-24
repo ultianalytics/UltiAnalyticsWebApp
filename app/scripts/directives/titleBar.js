e6529c2a7293823d163a5feaec0bcf0afd1a99c1
Kyle Geppert <Kyle.Geppert@docusign.com>
2014-06-24 03:43:29 -0700

    Hack fixed the mobile header. Shame is to be had.

--- app/scripts/directives/titleBar.js
+++ app/scripts/directives/titleBar.js
@@ -5,11 +5,24 @@ angular.module('newBetaApp')
     return {
       templateUrl: 'includes/partials/title-bar.html',
       restrict: 'EA',
-      link: function postLink(scope) {
+      link: function postLink(scope, element) {
+        var positionLogo = _.debounce(function(){
+          var $uaLogo = $(element).find('.ua-logo');
+          var $teamName = $(element).find('.navbar-brand');
+          if ($($teamName.offsetParent()).width() - $teamName.offset().left - $teamName.outerWidth() - 22 < $uaLogo.outerWidth() ) { // I'm sorry father for I ahve sinned.
+            $uaLogo.addClass('small-screen')
+          } else {
+            $uaLogo.removeClass('small-screen')
+          }
+        }, 75);
         if ( !_($routeParams).isEmpty() ){
           teamName.then(function(name){
             scope.teamName = name;
-            window.document.title = name || 'iUltimate';
+            window.document.title = name || 'ultiAnalytics';
+            $(window).on('resize',function(){
+              positionLogo();
+            });
+            positionLogo()
           });
         }
       }
