a9f39ea41b8f49f1e27f4f7dc32e160feb468623
Kyle Geppert <Kyle.Geppert@docusign.com>
2014-07-03 13:53:24 -0700

    Updated build

--- build.bash
+++ build.bash
@@ -1,5 +1,11 @@
+coffee -c .;
 grunt better_build;
+cd /Users/kyle.geppert/Code/Summit\ Hill\ Software/ultimateweb/UltimateWeb;
+git checkout master;
 cp -avR /Users/kyle.geppert/Code/Summit\ Hill\ Software/iUltimateWebApp/dist /Users/kyle.geppert/Code/Summit\ Hill\ Software/ultimateweb/UltimateWeb/war;
 rm -r /Users/kyle.geppert/Code/Summit\ Hill\ Software/ultimateweb/UltimateWeb/war/app;
 mv /Users/kyle.geppert/Code/Summit\ Hill\ Software/ultimateweb/UltimateWeb/war/dist /Users/kyle.geppert/Code/Summit\ Hill\ Software/ultimateweb/UltimateWeb/war/app;
-sublime /Users/kyle.geppert/Code/Summit\ Hill\ Software/ultimateweb/UltimateWeb/war/WEB-INF/appengine-web.xml;
+git add .;
+git commit -m "Built.";
+git pull -r;
+git push origin master;
